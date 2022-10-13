d1f6f3e206067826bc5991415be6f2c0ea739405
Millie Burhol Austad <millieba@stud.ntnu.no>
2022-10-13 20:31:43 +0200

    Added insert_activity + insert_trackpoint function

--- part-1.py
+++ part-1.py
@@ -14,8 +14,31 @@ class Program:
             '_id': id,
             'has_labels': has_labels
         }
-        self.db.User.insert_one(doc)
+        self.db.User.replace_one({'_id':id}, doc, upsert=True) # if user exists update with doc, else insert doc
+        #self.db.User.insert_one(doc)
         
+    def insert_activity(self, user_id, transportation_mode, start_date_time, end_date_time):       
+        doc = {
+        "user_id": user_id, # because _id is not set, mongodb will set _id to unique objectID
+        "transportation_mode": transportation_mode,
+        "start_date_time": start_date_time,
+        "end_date_time": end_date_time
+        }
+        self.db.Activity.replace_one({'user_id':user_id}, doc, upsert=True) # burde vel egt heller query _id siden den er unik
+
+    # note til austad, user 175 har ikke så mange plt filer og den har labels så fin for testing
+    
+    def insert_trackpoint(self, currentID):
+        doc = {
+        "activity_id": None, # none er bare placeholder atm
+        "lat": None,
+        "lon": None,
+        "altitude": None,
+        "date_time": None
+        }
+        self.db.TrackPoint.replace_one({'_id':currentID}, doc, upsert=True)
+       
+
     def fetch_documents(self, collection_name):
         collection = self.db[collection_name]
         documents = collection.find({})
@@ -38,7 +61,9 @@ def main():
     program = None
     try:
         program = Program()
-        program.insert_user("000", True)
+        program.insert_user("001", True)
+        #program.insert_user("000", False)
+        program.insert_activity("000", None, "2009-10-11, 14:04:30", "2009-10-11, 14:04:35")
     except Exception as e:
         print("ERROR: Failed to use database:", e)
     finally:
