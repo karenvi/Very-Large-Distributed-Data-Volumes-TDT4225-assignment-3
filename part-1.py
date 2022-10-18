033302c7ad49d2e34eaca29025a45b3d3c7ac7ae
Magnus Nordahl <54741107+magnusnor@users.noreply.github.com>
2022-10-18 15:42:00 +0200

    Embed Activity in User document

--- part-1.py
+++ part-1.py
@@ -3,6 +3,7 @@ from DbConnector import DbConnector
 import pandas as pd
 import os
 from decouple import config
+from bson.objectid import ObjectId
 
 
 class Program:
@@ -12,10 +13,11 @@ class Program:
         self.client = self.connection.client
         self.db = self.connection.db
     
-    def insert_user(self, id, has_labels):
+    def insert_user(self, id, has_labels, activities):
         doc = {
             '_id': id,
-            'has_labels': has_labels
+            'has_labels': has_labels,
+            'activities': activities
         }
         self.db.User.replace_one({'_id':id}, doc, upsert=True) # if user exists update with doc, else insert doc
         #self.db.User.insert_one(doc)
@@ -77,74 +79,128 @@ class Program:
 
             # Check if user is in labeled_ids.txt
             if user in labeled_ids.values:
-                self.insert_user(user, True)
+                user_has_labels = True
             else:
-                self.insert_user(user, False)
+                user_has_labels = False
 
-            # user_dir = f'{dataset_path}/Data/{user}/{"Trajectory"}'
+            user_dir = f'{dataset_path}/Data/{user}/{"Trajectory"}'
 
-            # # Iterate through all activities for a specific user
-            # for activity in tqdm(os.listdir(user_dir)):
-            #     plt_path = f'{user_dir}/{activity}'
-            #     file = pd.read_csv(plt_path, skiprows=6, header=None, parse_dates=[
-            #                     [5, 6]], infer_datetime_format=True)
+            activities = []
 
-            #     # Only insert activities with less than or equal 2500 trackpoints
-            #     if (len(file.index) <= 2500):
-            #         # Rename columns for clarity and remove unused columns
-            #         file.rename(inplace=True, columns={
-            #                     0: 'lat', 1: 'lon', 3: 'alt', 4: 'date_days', '5_6': 'date_time'})
-            #         file.drop(inplace=True, columns=[2])
+            # Iterate through all activities for a specific user
+            for activity in os.listdir(user_dir):
+                plt_path = f'{user_dir}/{activity}'
+                file = pd.read_csv(
+                    plt_path,
+                    skiprows=6,
+                    header=None,
+                    parse_dates=[[5, 6]],
+                    infer_datetime_format=True
+                )
 
-            #         # Fetch start and end time for the activity
-            #         start_date_time = pd.to_datetime(
-            #             file.head(1)['date_time'].values[0], format="%Y/%m/%d %H:%M:%S")
-            #         end_date_time = pd.to_datetime(
-            #             file.tail(1)['date_time'].values[0], format="%Y/%m/%d %H:%M:%S")
+                # Only insert activities with less than or equal 2500 trackpoints
+                if (len(file.index) <= 2500):
+                    # Rename columns for clarity and remove unused columns
+                    file.rename(
+                        inplace=True,
+                        columns={
+                            0: 'lat',
+                            1: 'lon',
+                            3: 'alt',
+                            4: 'date_days',
+                            '5_6': 'date_time'
+                        })
+                    file.drop(inplace=True, columns=[2])
 
-            #         if user in labeled_ids.values:
-            #             # Read labels.txt file and rename columns for clarity
-            #             labels = pd.read_csv(f'{os.path.dirname(user_dir)}/labels.txt', delim_whitespace=True,
-            #                                 skiprows=1, header=None, parse_dates=[[0, 1], [2, 3]], infer_datetime_format=True)
-            #             labels.rename(inplace=True, columns={
-            #                         '0_1': 'start_date_time', '2_3': 'end_date_time', 4: 'transportation_mode'})
+                    # Fetch start and end time for the activity
+                    start_date_time = pd.to_datetime(
+                        file.head(1)['date_time'].values[0], format="%Y/%m/%d %H:%M:%S"
+                    )
+                    end_date_time = pd.to_datetime(
+                        file.tail(1)['date_time'].values[0], format="%Y/%m/%d %H:%M:%S"
+                    )
 
-            #             # Match start time and end time in labels
-            #             matching_row = labels[((labels['start_date_time'] == start_date_time) & (
-            #                 labels['end_date_time'] == end_date_time))]
+                    if user in labeled_ids.values:
+                        # Read labels.txt file and rename columns for clarity
+                        labels = pd.read_csv(
+                            f'{os.path.dirname(user_dir)}/labels.txt',
+                            delim_whitespace=True,
+                            skiprows=1,
+                            header=None,
+                            parse_dates=[[0, 1], [2, 3]],
+                            infer_datetime_format=True
+                        )
+                        labels.rename(
+                            inplace=True,
+                            columns={
+                                '0_1': 'start_date_time',
+                                '2_3': 'end_date_time',
+                                4: 'transportation_mode'
+                            }
+                        )
 
-            #             # Check if there is a match
-            #             if len(matching_row) > 0:
-            #                 transportation_mode = matching_row['transportation_mode'].values[0]
-            #                 self.insert_activity(
-            #                     user, transportation_mode, start_date_time, end_date_time)
-            #                 activity_id = self.fetch_last_insert_id()
-            #                 file['activity_id'] = activity_id
-            #                 self.insert_track_points_batch(
-            #                     list(file.itertuples(index=False, name=None)))
-            #             else:
-            #                 self.insert_activity(
-            #                     user, 'NULL', start_date_time, end_date_time)
-            #                 activity_id = self.fetch_last_insert_id()
-            #                 file['activity_id'] = activity_id
-            #                 self.insert_track_points_batch(
-            #                     list(file.itertuples(index=False, name=None)))
-            #         else:
-            #             self.insert_activity(
-            #                 user, 'NULL', start_date_time, end_date_time)
-            #             activity_id = self.fetch_last_insert_id()
-            #             file['activity_id'] = activity_id
-            #             self.insert_track_points_batch(
-            #                 list(file.itertuples(index=False, name=None)))
+                        # Match start time and end time in labels
+                        matching_row = labels[(
+                            (labels['start_date_time'] == start_date_time) & 
+                            (labels['end_date_time'] == end_date_time)
+                        )]
 
+                        # Check if there is a match
+                        if len(matching_row) > 0:
+                            transportation_mode = matching_row['transportation_mode'].values[0]
+                            activity_id = ObjectId()
+                            activity = {
+                                "_id": activity_id,
+                                "transportation_mode": transportation_mode,
+                                "start_date_time": start_date_time,
+                                "end_date_time": end_date_time
+                            }
+                            activities.append(activity)
+                            # self.insert_activity(
+                            #     user, transportation_mode, start_date_time, end_date_time)
+                            # activity_id = self.fetch_last_insert_id()
+                            # file['activity_id'] = activity_id
+                            # self.insert_track_points_batch(
+                            #     list(file.itertuples(index=False, name=None)))
+                        else:
+                            # self.insert_activity(
+                            #     user, 'NULL', start_date_time, end_date_time)
+                            # activity_id = self.fetch_last_insert_id()
+                            # file['activity_id'] = activity_id
+                            # self.insert_track_points_batch(
+                            #     list(file.itertuples(index=False, name=None)))
+                            activity_id = ObjectId()
+                            activity = {
+                                "_id": activity_id,
+                                "transportation_mode": "NULL",
+                                "start_date_time": start_date_time,
+                                "end_date_time": end_date_time
+                            }
+                            activities.append(activity)
+                    else:
+                        # self.insert_activity(
+                        #     user, 'NULL', start_date_time, end_date_time)
+                        # activity_id = self.fetch_last_insert_id()
+                        # file['activity_id'] = activity_id
+                        # self.insert_track_points_batch(
+                        #     list(file.itertuples(index=False, name=None)))
+                        activity_id = ObjectId()
+                        activity = {
+                            "_id": activity_id,
+                            "transportation_mode": "NULL",
+                            "start_date_time": start_date_time,
+                            "end_date_time": end_date_time
+                        }
+                        activities.append(activity)
+                self.insert_user(user, user_has_labels, activities)
          
 
 def main():
     program = None
     try:
         program = Program()
-        program.insert_activity("000", None, "2009-10-11, 14:04:30", "2009-10-11, 14:04:35")
-        program.fetch_last_insert_id("Activity")
+        #program.insert_activity("000", None, "2009-10-11, 14:04:30", "2009-10-11, 14:04:35")
+        #program.fetch_last_insert_id("Activity")
         
         program.insert_dataset()
         #program.drop_coll("User")
